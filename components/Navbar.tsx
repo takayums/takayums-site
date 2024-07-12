8960ffd6c547f8fb39899fc4957647b72075f313
takayums <takayums20@gmail.com>
2024-07-12 17:33:48 +0700

    feat(navbar): responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,52 +1,54 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Bars3BottomLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
   const pathname = usePathname();
-  console.log(pathname);
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const menu = [
+    { href: "/", title: "home" },
+    { href: "/projects", title: "works" },
+    { href: "/about", title: "about me" },
+    { href: "/contacts", title: "contacts" },
+  ];
+
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
-    <header className="pt-8 pb-2">
+    <header className="py-8">
       <div className="container px-4 mx-auto max-w-screen-lg">
-        <nav className="flex justify-between">
+        <nav className="flex items-center justify-between relative">
           <Link href="/" className="text-white font-bold text-base">
             yums_
           </Link>
-          <ul className="text-base flex gap-8">
-            <li>
-              <Link
-                href="/"
-                className={`text-gray hover:font-medium hover:text-white ${pathname === "/" ? "font-medium text-white" : ""}`}
-              >
-                <span className="text-primary">#</span>home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/projects"
-                className={`text-gray hover:font-medium hover:text-white ${pathname === "/projects" ? "font-medium text-white" : ""}`}
-              >
-                <span className="text-primary">#</span>works
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className={`text-gray hover:font-medium hover:text-white ${pathname === "/about" ? "font-medium text-white" : ""}`}
-              >
-                <span className="text-primary">#</span>about-me
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/contacts"
-                className={`text-gray hover:font-medium hover:text-white ${pathname === "/contacts" ? "font-medium text-white" : ""}`}
-              >
-                <span className="text-primary">#</span>contacts
-              </Link>
-            </li>
+          <div className="md:hidden">
+            <button onClick={handleToggleMenu}>
+              {toggleMenu ? (
+                <Bars3BottomLeftIcon className="size-5 text-white" />
+              ) : (
+                <XMarkIcon className="size-5 text-white" />
+              )}
+            </button>
+          </div>
+          <ul
+            className={`text-base absolute top-10 right-0 py-2 bg-background md:static md:flex md:flex-row border border-gray md:border-none ${toggleMenu ? "hidden" : "top-[-100]"}`}
+          >
+            {menu.map((item, index) => (
+              <li key={index} onClick={handleToggleMenu}>
+                <Link
+                  href={item.href}
+                  className={`text-gray hover:font-medium hover:text-white hover:underline px-4 py-1.5 ${pathname === item.href ? "font-medium text-white" : ""}`}
+                >
+                  <span className="text-primary">#</span>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
