089556ac2b059e31c4c7b64876cd9909acb00d97
takayums <takayums20@gmail.com>
2024-07-17 12:24:06 +0700

    feat(projects): responsive mobile projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function Projects() {
           <h2 className="font-medium text-3xl text-white mb-5">
             <span className="text-primary">#</span>complete-apps
           </h2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <CardProject />
             <CardProject />
             <CardProject />
@@ -28,7 +28,7 @@ export default function Projects() {
           <h2 className="font-medium text-3xl text-white mb-5">
             <span className="text-primary">#</span>small-projects
           </h2>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <SmallProject />
             <SmallProject />
             <SmallProject />
