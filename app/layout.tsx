5686a2c7d251ec60a42798987a3ccbb0007a9ddb
takayums <takayums20@gmail.com>
2025-03-20 13:27:20 +0700

    refactory(darkmode): change system theme to false

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Fira_Code } from "next/font/google";
-import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
+import "./globals.css";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
@@ -18,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`${firaCode.className} flex flex-col min-h-screen`}>
-        <ThemeProvider attribute="class" enableSystem={true}>
+        <ThemeProvider attribute="class" enableSystem={false}>
           <Navbar />
           <main className="dark:bg-background bg-white grow flex flex-col">
             {children}
--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,24 +1,27 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Moon, Sun } from "react-feather";
 import { useTheme } from "next-themes";
 
 export default function ToggleTheme() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   const handleThemeThoggle = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="p-1 rounded-xl dark:bg-background border bg-gray-200 flex items-center">
       <button onClick={handleThemeThoggle}>
-        {theme == "dark" ? (
-          <Sun className="text-white" />
-        ) : (
-          <Moon stroke="gray" fill="gray" />
-        )}
+        {theme == "dark" ? <Sun fill="#fff" /> : <Moon fill="#282c33" />}
       </button>
     </div>
   );
